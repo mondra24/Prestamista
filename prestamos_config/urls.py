6abf62a1a8b817c96f28072f4794f60c2e80b95f
Ignacio Mondragon <ignaciomondragon24@gmail.com>
2026-02-12 13:30:35 -0300

    fix: agregar iconos PWA y favicon para resolver 404s

--- prestamos_config/urls.py
+++ prestamos_config/urls.py
@@ -7,10 +7,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
     path('', include('core.urls')),
 ]
 
